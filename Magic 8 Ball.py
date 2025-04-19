9580f48edc19a970475a9a394ebd53e1476a0504
HighPingLowIQ <radioactivebacon00@gmail.com>
2025-04-18 23:18:47 -0600

    Magic 8 Ball.py

---
+++ Magic 8 Ball.py
@@ -0,0 +1,28 @@
+# Write code below 💖
+
+import random
+
+num = random.randint(1, 9)
+
+print(input("What is your question to the allmighty 8-ball?: "))
+
+if num == 1:
+  print("Yes - Definitely")
+elif num == 2:
+  print("It is decidendly so")
+elif num == 3:
+  print("Without a doubt")
+elif num == 4:
+  print("Reply hazy, try again")
+elif num == 5:
+  print ("Ask again later.")
+elif num == 6:
+  print("Better not tell you now.")
+elif num == 7:
+  print("My sources say no")
+elif num == 8:
+  print("Outlook not so good")
+elif num == 9:
+  print("Very doubtful")
+else:
+  print("No.")
